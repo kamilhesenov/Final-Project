ba65bd8ffae20c468ad649644422ce9ef32a8b74
kamilhasanov <kamilah@code.edu.az>
2020-10-31 01:39:32 +0400

    Till Speciality-Section Font Part Created

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function (){
+
+//   Home-intro Corusel
     if($("#home-intro").length){
         $('.slider .owl-carousel').owlCarousel({
             margin: 30,
@@ -13,10 +15,12 @@ $(document).ready(function (){
             transitionStyle: "fade",
     })
     }
-$( ".slider .owl-carousel .owl-prev").html('<i class="fas fa-angle-left"></i>');
-$( ".slider .owl-carousel .owl-next").html('<i class="fas fa-angle-right"></i>');
+ $( ".slider .owl-carousel .owl-prev").html('<i class="fas fa-angle-left"></i>');
+ $( ".slider .owl-carousel .owl-next").html('<i class="fas fa-angle-right"></i>');
 
-function wowAnimation() {
+
+//  WowAnimation Plugin
+ function wowAnimation() {
     var wow = new WOW({
         boxClass: 'wow',
         animateClass: 'animated',
@@ -25,10 +29,68 @@ function wowAnimation() {
         live: true
     });
     wow.init();
-}
-$(window).on('load', function () {
+ }
+ $(window).on('load', function () {
   wowAnimation();
-});
+  });
+
+//   Home-services Corusel
+  if($("#services").length){
+    $('.services-corusel .owl-carousel').owlCarousel({
+        margin: 30,
+        items: 4,
+        dots: true,
+        loop: true,
+        nav: false,
+        mouseDrag: true,
+        navSpeed: 1000,
+        transitionStyle: "fade",
+        responsive: {
+                    0: {
+                        items: 1
+                    },
+                    768: {
+                        items: 2
+                    },
+                    992: {
+                        items: 4
+                    }
+                }
 })
+}
+$( ".services-coruse .owl-carousel .owl-dots .owl-dot").html('<span></span>');
+
+
+})
+
+// Home-Speciality
+var tablinks = document.querySelectorAll("#speciality .nav-pills .nav-item .nav-link");
+var tabContent = document.querySelectorAll("#speciality .tab-content");
+
+for(let elem of tablinks){
+    elem.addEventListener("click",function(e){
+        e.preventDefault();
+        
+      document.querySelector("#speciality .nav-pills .nav-item .nav-link.active").classList.remove("active");
+      document.querySelector(".tab-content.show").classList.remove("show");
+
+      elem.classList.add("active");
+
+      var count = elem.getAttribute("data-index");
+      var panel = [...tabContent].filter(elem => elem.getAttribute("data-index") == count);
+
+      panel[0].classList.add("show");
+      panel[0].classList.remove("d-none");
+
+    
+    for(let item of tabContent){
+      if(panel[0] !== item){
+        item.classList.add("d-none");
+      }
+    }
+      
+    })
+}
+
 
 
